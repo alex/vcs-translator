1447aaea0d4e96547bbadc1ddfa2a7c1dc685bad
Alex Gaynor <alex.gaynor@gmail.com>
2011-01-21 11:27:48 -0600

    Admin for feedback.

--- vcstranslator_project/apps/translator/admin.py
+++ vcstranslator_project/apps/translator/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 
-from translator.models import FailedTranslation
+from translator.models import FailedTranslation, TranslationFeedback
 
 
 admin.site.register(FailedTranslation,
-    list_display=["source", "target", "command", "count"],
+    list_display=["source", "command", "target", "count"],
     ordering=["-count"],
+)
+admin.site.register(TranslationFeedback,
+    list_display=["source", "command", "target", "comments"],
 )
